ccc5c4218207adc034413cc66239d82f70adce14
turtore <brunosap@gmail.com>
2024-11-11 23:26:11 -0300

    updated curriculum function

--- hello_genai.py
+++ hello_genai.py
@@ -3,7 +3,14 @@ import os
 
 
 genai.configure(api_key=os.environ["GEMINI_API"])
-
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Crie uma história sobre um computador mágico")
+# response = model.generate_content("Crie uma história sobre um computador mágico")
+
+
+with open("curriculo-joao-silva.txt") as file:
+    curriculum = file.read()
+
+response = model.generate_content(
+    f"Deixar o meu currículo mais resumido e assertivo:{curriculum}")
+
 print(response.text)
